e57ce3e8d8e40c25e6f62b2fcb70c1ff8a4e776a
Alex <alejandromirandaco1801@gmail.com>
2025-02-18 23:43:26 +0100

    feat: update welcome page layout and add portada image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,42 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-</head>
-
-<body class="antialiased"
-    style="background-image: url('{{ asset('images/imagen_portada.webp') }}'); background-size: cover; background-repeat: no-repeat;">
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        @if (Route::has('login'))
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                @auth
-                    <a href="{{ url('/home') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-                @else
-                    <a href="{{ route('login') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                        in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
-
+@section('content')
+<img src="images/imagen_portada.webp" class="d-flex w-100">
+<div class="class=" container mt-5">
+    <div class="d-flex justify-content-center bg-white bg-opacity-10 rounded-3 shadow-lg flex-column content-box">
+        <p class="text-center fs-1 fw-bolder">Virtual meditate</p>
+        <h2 class="text-center fw-bold">A momento of calm in a world of hurry</h2>
     </div>
-</body>
+</div>
 
-</html>
+
+@endsection
